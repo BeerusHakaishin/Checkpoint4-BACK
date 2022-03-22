1c6028ecc9cec80cf158f62c21f2f0db5480de58
Ismaël Meziani <meziani-i@outlook.fr>
2022-03-22 19:56:01 +0100

    authcrud

--- src/main/java/com/argonaute/checkpoint/api/controller/AuthController.java
+++ src/main/java/com/argonaute/checkpoint/api/controller/AuthController.java
@@ -1,9 +1,7 @@
 package com.argonaute.checkpoint.api.controller;
 
 import java.util.Date;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 import java.util.stream.Collectors;
 
 import javax.servlet.http.HttpServletRequest;
@@ -24,15 +22,12 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.argonaute.checkpoint.api.dto.UserDto;
 import com.argonaute.checkpoint.api.jwt.JwtUtils;
 import com.argonaute.checkpoint.api.model.BlackList;
-import com.argonaute.checkpoint.api.model.ERole;
-import com.argonaute.checkpoint.api.model.Role;
 import com.argonaute.checkpoint.api.model.User;
 import com.argonaute.checkpoint.api.payload.request.LoginRequest;
-import com.argonaute.checkpoint.api.payload.request.SignUpRequest;
 import com.argonaute.checkpoint.api.payload.response.JwtResponse;
-import com.argonaute.checkpoint.api.payload.response.MessageResponse;
 import com.argonaute.checkpoint.api.repository.BlackListRepository;
 import com.argonaute.checkpoint.api.repository.RoleRepository;
 import com.argonaute.checkpoint.api.repository.UsersRepository;
@@ -76,43 +71,11 @@ public class AuthController {
 	}
 
 	@PostMapping("/signup")
-	public ResponseEntity<?> registerUser(@Valid @RequestBody SignUpRequest signUpRequest) {
-		if (Boolean.TRUE.equals(userRepository.existsByUsername(signUpRequest.getUsername()))) {
-			return ResponseEntity.badRequest().body(new MessageResponse("Error: Username is already taken!"));
-		}
-
-		signUpRequest.setPassword("12345678");
-		// Create new user's account
-		User user = new User(signUpRequest.getUsername(), encoder.encode(signUpRequest.getPassword()));
-
-		Set<String> strRoles = signUpRequest.getRole();
-		Set<Role> roles = new HashSet<>();
-		String error = "Error: Role is not found.";
-
-		if (strRoles == null) {
-			Role userRole = roleRepository.findByName(ERole.ROLE_USER).orElseThrow(() -> new RuntimeException(error));
-			roles.add(userRole);
-		} else {
-			strRoles.forEach(role -> {
-				switch (role) {
-				case "admin":
-					Role adminRole = roleRepository.findByName(ERole.ROLE_ADMIN)
-							.orElseThrow(() -> new RuntimeException(error));
-					roles.add(adminRole);
-
-					break;
-				default:
-					Role userRole = roleRepository.findByName(ERole.ROLE_USER)
-							.orElseThrow(() -> new RuntimeException(error));
-					roles.add(userRole);
-				}
-			});
-		}
-
-		user.setRoles(roles);
-		userRepository.save(user);
-
-		return ResponseEntity.ok(new MessageResponse("User registered successfully!"));
+	public User createUser(@Valid @RequestBody UserDto userDto) {
+		User user = new User();
+		user.setUsername(userDto.getUsername());
+		user.setRoles(userDto.getRoles());
+		return userRepository.save(user);
 	}
 
 	@GetMapping("/istokenexpired")
--- src/main/java/com/argonaute/checkpoint/api/dto/UserDto.java
+++ src/main/java/com/argonaute/checkpoint/api/dto/UserDto.java
@@ -1,14 +1,31 @@
 package com.argonaute.checkpoint.api.dto;
 
+import java.util.HashSet;
 import java.util.Set;
 
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.Size;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+
+import com.argonaute.checkpoint.api.model.Role;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 public class UserDto {
-	@NotBlank
-	@Size(min = 2, max = 20)
-	private String username;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
 
 	public String getUsername() {
 		return username;
@@ -18,13 +35,29 @@ public class UserDto {
 		this.username = username;
 	}
 
-	public Set<String> getRole() {
-		return role;
+	public String getPassword() {
+		return password;
 	}
 
-	public void setRole(Set<String> role) {
-		this.role = role;
+	public void setPassword(String password) {
+		this.password = password;
 	}
 
-	private Set<String> role;
+	public Set<Role> getRoles() {
+		return roles;
+	}
+
+	public void setRoles(Set<Role> roles) {
+		this.roles = roles;
+	}
+
+	private String username;
+
+	@JsonIgnore
+	private String password;
+
+	@ManyToMany(fetch = FetchType.LAZY)
+	@JoinTable(name = "user_roles", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "role_id"))
+	private Set<Role> roles = new HashSet<>();
+
 }
